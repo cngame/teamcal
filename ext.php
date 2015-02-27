33027e11f2c7b13891071186ffb24901c3f19dfb
Alban Cucchieri <alban.cucchieri@equation.fr>
2015-02-27 17:22:14 +0100

    Initial

---
+++ ext.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * TeamCal extension package.
+ *
+ * @copyright (c) 2015 CN Game <https://github.com/cngame/>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace cngame\teamcal;
+
+class ext extends \phpbb\extension\base
+{
+}
